4e6bd19b05be5bda9546ee51b4a3ae1c4d5971a8
vxxgd <chillyill0@proton.me>
2025-08-31 12:07:50 +0500

    Add files via upload

---
+++ src/wiki.js
@@ -0,0 +1,39 @@
+/*
+ * @require /src/markdown.js
+ */ 
+
+let page;
+
+function exit(context) {
+  if (context === "NotFound") {
+    window.location.href = `/error/404?${page}`;
+  } else if (context === "NoPage") {
+    window.location.href = "/error/404";
+  } else {
+    window.location.href = "/";
+  }
+}
+
+const pageMatch = window.location.href.match(/https?:\/\/.*\?(.*)/);
+if (!pageMatch) exit("NoPage");
+page = pageMatch[1];
+if (!page) exit("NoPage");
+
+async function loadPage() {
+  const response = await fetch(`/content/${page}.md`);
+  if (response.status === 404) return exit("NotFound");
+  if (response.status !== 200) {
+    console.log(response);
+    document.body.innerText = "Unexpected error occured. Please check console for more details.";
+    return;
+  }
+  const content = await response.text();
+  const article = document.getElementById("article");
+  document.title = `${page.replaceAll("-", " ")} : GD Wiki`
+  article.innerHTML = window.markdown(content);
+  
+  // KaTeX integration
+  renderMathInElement(document.getElementById('article'));
+}
+
+loadPage();
